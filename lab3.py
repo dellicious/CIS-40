3fbfa0cdc451d0dfb53caab35e09c9019435bedb
dellicious <ka.ngcake@gmail.com>
2023-02-27 15:47:57 -0800

    Base Code Frame Complete. Error: Did not import random yet

--- lab3.py
+++ lab3.py
@@ -2,20 +2,28 @@
 # This program is a "guess the number" type game
 
 #Function that will get users to guess a number between 1 and 20
-
+targetNum = randint(1,20)
+userNum=input("Please enter a number between 1-20.")
 
 #CheckNum function that checks guess against target number.
-
+def checkNum():
+    if userNum < targetNum:
+        print("Your Number is too low.")
+    if userNum > targetNum:
+        print("Your number is too high.")
+    if userNum == targetNum:
+        print("You got it!")
 
 #Function that loops 5 times.
-targetNum = randrange(1,20)
-gameNum = 5
-if gameNum <= 0:
-    gameNum= gameNum-0
-if gameNum <0:
-    print("Sorry, you ran out of tries.")
+
+
 if checkNum != "You got it!":
     print("Sorry, wrong number. Let's try again.")
     print("You have", gameNum, "more tries.")
 if checkNum == "You got it!":
     print("Congratulations, you won!")
+gameNum = 5
+if gameNum > 0:
+    gameNum= gameNum-1
+if gameNum <=0:
+    print("Sorry, you ran out of tries.")
