54d75a4afb9fd2ff9210bd9bd5b318bb3b00f6f7
dellicious <ka.ngcake@gmail.com>
2023-02-27 15:51:58 -0800

    Game not stopping at 5 turns Currently the turn number gets overriden by input number. The checkNum function is not being printed. Got random working.

--- lab3.py
+++ lab3.py
@@ -1,8 +1,9 @@
 # Tiffany Kang
 # This program is a "guess the number" type game
 
+import random
 #Function that will get users to guess a number between 1 and 20
-targetNum = randint(1,20)
+targetNum = random.randrange(1,20)
 userNum=input("Please enter a number between 1-20.")
 
 #CheckNum function that checks guess against target number.
@@ -15,15 +16,15 @@ def checkNum():
         print("You got it!")
 
 #Function that loops 5 times.
-
+gameNum = 5
+if gameNum > 0:
+    gameNum= gameNum-1
+if gameNum ==0:
+    print("Sorry, you ran out of tries.")
 
 if checkNum != "You got it!":
     print("Sorry, wrong number. Let's try again.")
     print("You have", gameNum, "more tries.")
 if checkNum == "You got it!":
     print("Congratulations, you won!")
-gameNum = 5
-if gameNum > 0:
-    gameNum= gameNum-1
-if gameNum <=0:
-    print("Sorry, you ran out of tries.")
+
