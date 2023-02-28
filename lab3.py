d8a98efda4d032f79a6a4fbfdcc14c596e53d201
dellicious <ka.ngcake@gmail.com>
2023-02-27 23:07:49 -0800

    dad helped me :D problem with this code is it doesn't process checkNum he fixed the round#s by moving around the loop contents

--- lab3.py
+++ lab3.py
@@ -3,31 +3,38 @@
 
 import random
 #Function that will get users to guess a number between 1 and 20
-targetNum = random.randrange(1,20)
-userNum=int(input("Please enter a number between 1-20.\n"))
+
+def getInput(): 
+    userNum=int(input("Please enter a number between 1-20. \n"))
+    while userNum <=0 or userNum>=20:
+        userNum=int(input("I said, *please enter a number between 1-20. \n"))
+    return userNum
 
 #CheckNum function that checks guess against target number.
-def checkNum():
+def checkNum(userNum, targetNum):
     if userNum < targetNum:
-        print("Your Number is too low.")
+        return "Your Number is too low."
     if userNum > targetNum:
-        print("Your number is too high.")
+        return "Your number is too high."
     if userNum == targetNum:
-        print("You got it!")
+        return "You got it!"
 
 #Function that loops 5 times.
 def gameLoop(): 
+    targetNum = random.randrange(1,20)
     gameNum = 5
-    if gameNum > 0:
+    while gameNum > 0:
         gameNum= gameNum-1
-    if gameNum ==0:
+        userNum = getInput()
+
+        if checkNum(userNum, targetNum) == "You got it!":
+           print("Congratulations, you won!")
+           break
+        else:
+            print("Sorry, wrong number. Let's try again.")
+            print("You have", gameNum, "more tries.")
+            
+    if gameNum == 0:
         print("Sorry, you ran out of tries.")
 
-    if checkNum != "You got it!":
-        print("Sorry, wrong number. Let's try again.")
-        print("You have", gameNum, "more tries.")
-    if checkNum == "You got it!":
-        print("Congratulations, you won!")
-
-checkNum()
 gameLoop()
