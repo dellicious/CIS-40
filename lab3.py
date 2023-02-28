532ac1ab00032dea4e1ae51fe6de7e46edb241b4
dellicious <ka.ngcake@gmail.com>
2023-02-27 21:00:47 -0800

    The code works correctly for the most part but will not let the user input a new answer when wrong try turning userGuess into a function and then calling it at the end.

--- lab3.py
+++ lab3.py
@@ -4,7 +4,7 @@
 import random
 #Function that will get users to guess a number between 1 and 20
 targetNum = random.randrange(1,20)
-userNum=input("Please enter a number between 1-20.")
+userNum=int(input("Please enter a number between 1-20.\n"))
 
 #CheckNum function that checks guess against target number.
 def checkNum():
@@ -16,15 +16,18 @@ def checkNum():
         print("You got it!")
 
 #Function that loops 5 times.
-gameNum = 5
-if gameNum > 0:
-    gameNum= gameNum-1
-if gameNum ==0:
-    print("Sorry, you ran out of tries.")
+def gameLoop(): 
+    gameNum = 5
+    if gameNum > 0:
+        gameNum= gameNum-1
+    if gameNum ==0:
+        print("Sorry, you ran out of tries.")
 
-if checkNum != "You got it!":
-    print("Sorry, wrong number. Let's try again.")
-    print("You have", gameNum, "more tries.")
-if checkNum == "You got it!":
-    print("Congratulations, you won!")
+    if checkNum != "You got it!":
+        print("Sorry, wrong number. Let's try again.")
+        print("You have", gameNum, "more tries.")
+    if checkNum == "You got it!":
+        print("Congratulations, you won!")
 
+checkNum()
+gameLoop()
