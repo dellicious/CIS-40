c42a1e4fdfa4604ef0c14e4dd314df9b5cb65337
dellicious <ka.ngcake@gmail.com>
2023-03-06 17:45:26 -0800

    Completed Part 2

--- lab4.py
+++ lab4.py
@@ -9,3 +9,10 @@
 
 
 #Part2
+numList1 = [1,2,3,4,5]
+numList1.insert(0, numList1.pop(numList1.index(5)))
+print(numList1)
+
+numList2 = [5,2,3,4,11,9,20]
+numList2.insert(0,numList2.pop(numList2.index(20)))
+print(numList2)
