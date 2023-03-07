fd7085cd89f71b5d38c224903e15e9c2c7524609
dellicious <ka.ngcake@gmail.com>
2023-03-06 19:11:00 -0800

    added input ability to part 2 need to get rid of brackets somehow :()

--- lab4.py
+++ lab4.py
@@ -5,14 +5,34 @@
 
 
 #Part 1
-
+palindromechk = ["seve!n", "racecar"]
+for word in palindromechk:
+    if word != "," or "." or "?" or "!" or '"' or "'":
+        if word() == word[-1]:
+            print(True)
 
 
 #Part2
+numList=[]
+response=0
+response= input("Please enter an interger, or 'n' to quit.")
+while response != "n":
+    response= input("Please enter an interger, or 'n' to quit.")
+    if response != "n":
+        numList.append(int(response))
+print("List Input:",numList)
+numList.insert(0,numList[-1:])
+numList.pop()
+print("List Output:",numList)
+            
 numList1 = [1,2,3,4,5]
-numList1.insert(0, numList1.pop(numList1.index(5)))
-print(numList1)
+print("Example 1 Input:",numList1)
+numList1.insert(0,numList1[-1:])
+numList1.pop()
+print("Example 1 Output:",numList1)
 
-numList2 = [5,2,3,4,11,9,20]
-numList2.insert(0,numList2.pop(numList2.index(20)))
-print(numList2)
+numList2 = [9, 10, 7, 8, 11, 5, 2]
+print("Example 2 Input:",numList2)
+numList2.insert(0,numList2[-1:])
+numList2.pop()
+print("Example 2 Output:",numList2)
